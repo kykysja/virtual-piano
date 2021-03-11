1ab5b6b65c3aa330b1406a2fa49122a1daa008e9
kykysja <kykysja11@gmail.com>
2021-03-11 15:54:47 +0100

    feat: implement sound playing on mouse click

---
+++ virtual-piano/index.js
@@ -0,0 +1,23 @@
+const piano = document.querySelector('.piano');
+const pianoKeys = document.querySelectorAll('piano-key');
+
+function playAudio(audio) {
+	if (!audio) return;
+	audio.currentTime = 0;
+	audio.play();
+}
+
+piano.addEventListener('mousedown', function (event) {
+
+	const dataLetter = event.target.dataset.letter;
+	const audio = document.querySelector(`audio[data-letter="${dataLetter}"]`);
+
+	event.target.classList.add('piano-key-active');
+
+	window.addEventListener('mouseup', function () {
+		event.target.classList.remove('piano-key-active');
+	});
+
+	playAudio(audio);
+})
+
