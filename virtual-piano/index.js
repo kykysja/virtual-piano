44429e41e4f8d82415c574fd23a4670bf9f2ec8b
kykysja <kykysja11@gmail.com>
2021-03-11 16:02:56 +0100

    feat: implement sound playing on keypress

--- virtual-piano/index.js
+++ virtual-piano/index.js
@@ -21,3 +21,17 @@ piano.addEventListener('mousedown', function (event) {
 	playAudio(audio);
 })
 
+window.addEventListener('keydown', function (event) {
+	if (event.code[0] === 'K') {
+		const dataLetter = event.code[3];
+		const audio = document.querySelector(`audio[data-letter="${dataLetter}"]`);
+		playAudio(audio);
+		const element = document.querySelector(`div[data-letter='${dataLetter}']`);
+		if (element) {
+			element.classList.add('piano-key-active');
+			window.addEventListener('keyup', function (event) {
+				element.classList.remove('piano-key-active');
+			});
+		}
+	}
+})
